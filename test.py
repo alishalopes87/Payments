2fe32d13938fe7c51ce2b5423510efb6f35c0744
Alisha Lopes <alishalopes@MacBook-Pro-3.home>
2019-04-05 20:16:12 -0700

    Added testing

--- test.py
+++ test.py
@@ -1,30 +1,58 @@
 import unittest 
 from payment_application import *
 from payment_processor import PaymentProcessor
+from user_database import *
+import sys
+import logging
 # https://realpython.com/python-testing/#more-advanced-testing-scenarios
 #added unit tests 
 
-class Test(unittest.TestCase):
+
+logger = logging.getLogger()
+logger.level = logging.DEBUG
+stream_handler = logging.StreamHandler(sys.stdout)
+logger.addHandler(stream_handler)
+
+
+
+class TestPaymentProcessor(unittest.TestCase):
     "Tests for site."
 
     # def setUpClass(cls):
     #     cls._connection = createExpensiveConnectionObject()
+    def testSimpleMsg(self):
+        stream_handler.stream = sys.stdout
+        print("AA")
+        logging.getLogger().info("BB")
 
-    def success(self):
+    def test_payment_success(self):
         """
         Test that payment was successful
 
         """
+        processor = PaymentProcessor()
         result = processor.process_payment(incoming_request)
         self.assertTrue(result)
 
+    def test_incoming_request(self):
+        incoming_request = IncomingRequest()
+        incoming_request.user_id = 1;
+        incoming_request.user_name = "ABC";
+        incoming_request.billing_address = "123 Some Street, City Name, ST";
+        incoming_request.amount = 1;
+        #added card_number attritbute to validate
+        incoming_request.card_number = "3716820019271998" 
+        assert incoming_request.amount > 0
 
-    # def tearDownClass(cls):
-    #     cls._connection.destroy()
+    def test_new_user(self):
+        user_database = UserDatabase()
+
+        user_database.add_new_user(1, "Alisha Lopes", "200 Shady Acres")
+        self.assertTrue(1 in user_database.user_names)
+        
 
 
 
 if __name__ == "__main__":
     unittest.main()
-    "YAY ALL TESTS PASSED!"
     
--- user_database.py
+++ user_database.py
@@ -6,17 +6,18 @@ class UserDatabase(object):
         self.readDb()
 
     def readDb(self):
-        for row in open("seed_data/user_database.txt"):
+        f = open("seed_data/user_database.txt")
+        for row in f:
             splits = row.strip().split("\t")
             integer = int(splits[0])
 
             self.user_names[integer] = splits[1]
             self.addresses[integer] = splits[2]
 
+        f.close()
 
     def add_new_user(self,userId, username, address):
         self.user_names[userId] = username
         self.addresses[userId] = address
-
 
 
